2851ad5631a69d3c6304a3f2882d431d6af90cfe
Finneas Winslow Snow <finn@Finneass-MacBook-Pro.local>
2026-09-05 15:29:07 -0400

    init commit

--- hw1.py
+++ hw1.py
@@ -1,16 +1,16 @@
 # TODO: Edit these variables so they store your name and your hometown.
-my_name = "Laura South"
-my_hometown = "Denver, CO"
+my_name = "Finn Snow"
+my_hometown = "Portland, ME"
 
 print(f"Hello, my name is {my_name} and I am from {my_hometown}")
 # TODO: Write a print statement here that explains why you chose to take this course. 
-print()
+print("I am taking CS760 because I want to refine my design skills and apply it to my software development in industry")
 
 # TODO: Write a print statement here that describes how comfortable you are programming in Python
-print()
+print("I am very comfortable programming in python")
 
 # TODO: Write a print statement here that describes how comfortable you are writing code in HTML and JavaScript
-print()
+print("I am very comfortable programming in HTML/JS. Less than python")
 
 # TODO: Write a print statement here that describes how comfortable you are with writing assignments (in English!)
-print()
+print("I am very comfortable with English writing assignments")
